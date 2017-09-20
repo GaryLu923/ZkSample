c7c03798dc87521c1d5dc4035590e5673716e407
gary <howard9232002@gmail.com>
2017-09-21 03:04:19 +0800

    [Add] login page of zul
    Signed-off-by: gary <howard9232002@gmail.com>

---
+++ src/main/java/org/gary/ZkSample/controller/LoginController.java
@@ -0,0 +1,8 @@
+package org.gary.ZkSample.controller;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.SelectorComposer;
+
+public class LoginController extends SelectorComposer<Component> {
+
+}
